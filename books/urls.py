83318e077f4e300648ff4a7db2e6f2d1587a0000
puzzelchannel <puzzel.channel@gmail.com>
2022-12-12 18:35:20 +0330

    add update and delete view

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book-list'),
-    path('<int:pk>/book-detail', BookDetailView.as_view(), name='book-detail'),
+    path('<int:pk>/book-detail/', BookDetailView.as_view(), name='book-detail'),
+    path('book-create/', BookCreateView.as_view(), name='book-create'),
+    path('<int:pk>/book-update/', BookUpdateView.as_view(), name='book-update'),
+    path('<int:pk>/book-delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from .models import Book
 from django.views import generic
+from django.urls import reverse_lazy
+from .models import Book
+
 
 # Create your views here.
 
@@ -9,6 +10,25 @@ class BookListView(generic.ListView):
     template_name = 'books/book_list.html'
     context_object_name = 'books'
 
+
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'description', 'author', 'price']
+    template_name = 'books/book_create.html'
+
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'description', 'author', 'price']
+    template_name = 'books/book_update.html'
+
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy('book-list')
+
