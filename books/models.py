a8f5706506a1a3c05bcc0588a5c8b1696626e133
puzzelchannel <puzzel.channel@gmail.com>
2022-12-13 15:59:11 +0330

    add comment system for books

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
+
 
 # Create your models here.
 
@@ -9,9 +11,19 @@ class Book(models.Model):
     description = models.TextField()
     price = models.PositiveIntegerField()
     cover = models.ImageField(upload_to='covers/', blank=True)
+
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('book-detail', args=[self.id])
 
+
+class Comment(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, related_name='comments', on_delete=models.CASCADE)
+    text = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
